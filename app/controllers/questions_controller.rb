bde0c7078d78d59ba960ffc9db059a96a22a1e86
zac nation <zac.nation@icloud.com>
2023-02-20 15:03:00 +0000

    solution 1

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,5 +3,14 @@ class QuestionsController < ApplicationController
   end
 
   def answer
+    @message = params[:question]
+
+    if @message == "I am going to work"
+      @response = "Great!"
+    elsif @message.end_with?("?")
+      @response = "Silly question, get dressed and go to work!"
+    else
+      @response = "I don't care, get dressed and go to work!"
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "questions#ask"
   get "/ask", to: "questions#ask"
   get "/answer", to: "questions#answer"
   # Defines the root path route ("/")
